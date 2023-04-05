2a23141f4c90efc463e85d3497ec435c7a4dc04b
Zach Walsh <zach.c.walsh@gmail.com>
2023-04-05 06:24:42 -0400

    Use shorter tokens

--- src/main/kotlin/sh/zachwal/button/auth/contact/ContactTokenStore.kt
+++ src/main/kotlin/sh/zachwal/button/auth/contact/ContactTokenStore.kt
@@ -2,11 +2,13 @@ package sh.zachwal.button.auth.contact
 
 import org.slf4j.LoggerFactory
 import sh.zachwal.button.db.dao.ContactTokenDAO
+import java.security.SecureRandom
 import java.time.Instant
 import java.time.temporal.ChronoUnit
 import java.util.UUID
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.streams.toList
 
 // Custom class that stores a token -> contact mapping
 @Singleton
@@ -16,13 +18,25 @@ class ContactTokenStore @Inject constructor(
 
     private val logger = LoggerFactory.getLogger(ContactTokenStore::class.java)
 
+    private val secureRandom = SecureRandom()
+    private val chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    private fun newToken(): String {
+        return secureRandom.ints(20, 0, chars.length).toList()
+            .map { randInt ->
+                chars[randInt]
+            }.fold(StringBuilder()) { sb, char ->
+                sb.append(char)
+            }.toString()
+    }
+
     fun createToken(contactId: Int): String {
-        val newToken = UUID.randomUUID().toString()
+        val token = newToken()
         val expiration = Instant.now().plus(7, ChronoUnit.DAYS)
-        val contactToken = contactTokenDAO.createToken(newToken, contactId, expiration)
+        val contactToken = contactTokenDAO.createToken(token, contactId, expiration)
 
         logger.info("Stored contact token $contactToken.")
-        return newToken
+        return token
     }
 
     fun checkToken(token: String): Int? {
