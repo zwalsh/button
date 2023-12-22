62bc066b5262fcae79fd733a21715f91a60b6691
Zach Walsh <zach.c.walsh@gmail.com>
2023-12-22 15:28:21 -0500

    Tests of hour of day

--- src/test/kotlin/sh/zachwal/button/wrapped/WrappedServiceTest.kt
+++ src/test/kotlin/sh/zachwal/button/wrapped/WrappedServiceTest.kt
@@ -5,8 +5,8 @@ import io.mockk.mockk
 import org.junit.jupiter.api.Test
 import sh.zachwal.button.db.dao.PressDAO
 import sh.zachwal.button.db.jdbi.Press
-import java.time.DayOfWeek
 import java.time.Instant
+import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
 import kotlin.test.assertEquals
 
@@ -45,7 +45,51 @@ class WrappedServiceTest {
 
         val wrapped = service.wrapped(2023, "1")
 
-        assertEquals(DayOfWeek.SUNDAY, wrapped.favoriteDay)
+        assertEquals("Sunday", wrapped.favoriteDay)
         assertEquals(2, wrapped.favoriteDayCount)
+    }
+
+    @Test
+    fun `includes favorite hour with count`() {
+        val sundayNoon = Instant.from(
+            DateTimeFormatter.ISO_ZONED_DATE_TIME.parse("2023-12-10T12:00:00-05:00")
+        )
+
+        val sundayOne = sundayNoon.plus(1, ChronoUnit.HOURS)
+        val p1 = Press(time = sundayNoon, "", 1)
+        val p2 = p1.copy(time = sundayOne)
+        val p3 = p1.copy(time = sundayNoon.plus(1, ChronoUnit.DAYS))
+        every { dao.selectBetweenForContact(any(), any(), any()) } returns listOf(p1, p2, p3)
+
+        val wrapped = service.wrapped(2023, "1")
+
+        assertEquals("12PM", wrapped.favoriteHourString)
+        assertEquals(2, wrapped.favoriteHourCount)
+    }
+
+    @Test
+    fun `formats favorite hour when 12AM`() {
+        val midnight = Instant.from(
+            DateTimeFormatter.ISO_ZONED_DATE_TIME.parse("2023-12-10T00:00:00-05:00")
+        )
+        val p1 = Press(time = midnight, "", 1)
+        every { dao.selectBetweenForContact(any(), any(), any()) } returns listOf(p1)
+
+        val wrapped = service.wrapped(2023, "1")
+
+        assertEquals("12AM", wrapped.favoriteHourString)
+    }
+
+    @Test
+    fun `formats favorite hour when 11PM`() {
+        val midnight = Instant.from(
+            DateTimeFormatter.ISO_ZONED_DATE_TIME.parse("2023-12-10T23:00:00-05:00")
+        )
+        val p1 = Press(time = midnight, "", 1)
+        every { dao.selectBetweenForContact(any(), any(), any()) } returns listOf(p1)
+
+        val wrapped = service.wrapped(2023, "1")
+
+        assertEquals("11PM", wrapped.favoriteHourString)
     }
 }
