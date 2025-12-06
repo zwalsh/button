e8c081f6af716350e7ba00548c4a29a96f3ce200
Zach Walsh <zach.c.walsh@gmail.com>
2025-12-06 15:04:34 -0500

    PR2: Add contact_press_counts table, DAO, and integration tests for SMS prioritization plan [2025-12-06]

---
+++ src/main/kotlin/sh/zachwal/button/db/dao/ContactPressCountDAO.kt
@@ -0,0 +1,45 @@
+package sh.zachwal.button.db.dao
+
+import org.jdbi.v3.sqlobject.customizer.Bind
+import org.jdbi.v3.sqlobject.customizer.BindBean
+import org.jdbi.v3.sqlobject.statement.SqlQuery
+import org.jdbi.v3.sqlobject.statement.SqlUpdate
+import sh.zachwal.button.db.jdbi.ContactPressCount
+import java.time.LocalDate
+import org.jdbi.v3.sqlobject.config.KeyColumn
+import org.jdbi.v3.sqlobject.config.ValueColumn
+
+interface ContactPressCountDAO {
+    @SqlUpdate("""
+        insert into contact_press_counts (contact_id, date, press_count)
+        values (:contactId, :date, :pressCount)
+        on conflict (contact_id, date) do update set press_count = excluded.press_count
+    """)
+    fun upsert(@BindBean contactPressCount: ContactPressCount)
+
+    @SqlQuery("""
+        select contact_id, date, press_count
+        from contact_press_counts
+        where contact_id = :contactId and date = :date
+    """)
+    fun find(contactId: Int, date: LocalDate): ContactPressCount?
+
+    @SqlQuery("""
+        select contact_id, date, press_count
+        from contact_press_counts
+        where contact_id = :contactId
+        order by date desc
+    """)
+    fun findAllForContact(contactId: Int): List<ContactPressCount>
+
+
+    @KeyColumn("contact_id")
+    @ValueColumn("press_count")
+    @SqlQuery("""
+        select contact_id, sum(press_count) as press_count
+        from contact_press_counts
+        where date >= :startDate and date <= :endDate
+        group by contact_id
+    """)
+    fun aggregateCountsByContact(startDate: java.time.LocalDate, endDate: java.time.LocalDate): Map<Int, Int>
+}
