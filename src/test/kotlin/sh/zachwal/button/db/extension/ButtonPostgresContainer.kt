785c1c1ae2082fc4cc889f08d7b5e9fe0471c8d7
Zach Walsh <zach.c.walsh@gmail.com>
2026-04-19 16:18:03 -0400

    Upgrade more versions

--- src/test/kotlin/sh/zachwal/button/db/extension/ButtonPostgresContainer.kt
+++ src/test/kotlin/sh/zachwal/button/db/extension/ButtonPostgresContainer.kt
@@ -5,7 +5,7 @@ import org.testcontainers.utility.DockerImageName
 import java.sql.Connection
 import java.sql.DriverManager
 
-class ButtonPostgresContainer : PostgreSQLContainer<ButtonPostgresContainer>(DockerImageName.parse("postgres:12.18")) {
+class ButtonPostgresContainer : PostgreSQLContainer<ButtonPostgresContainer>(DockerImageName.parse("postgres:17")) {
     fun jdbcConnection(): Connection {
         return DriverManager.getConnection(
             jdbcUrl,
