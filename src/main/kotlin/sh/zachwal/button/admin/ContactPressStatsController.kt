06e5e593f8a270f0b92572dd475ffbee30c592e8
zwalsh <46062132+zwalsh@users.noreply.github.com>
2026-05-23 12:32:56 -0400

    Add leaderboard page for contacts (#107)

--- src/main/kotlin/sh/zachwal/button/admin/ContactPressStatsController.kt
+++ src/main/kotlin/sh/zachwal/button/admin/ContactPressStatsController.kt
@@ -6,13 +6,17 @@ import io.ktor.server.html.respondHtml
 import io.ktor.server.routing.Routing
 import io.ktor.server.routing.get
 import kotlinx.html.FormMethod
+import kotlinx.html.HTML
 import kotlinx.html.body
 import kotlinx.html.div
 import kotlinx.html.form
 import kotlinx.html.h1
 import kotlinx.html.head
+import kotlinx.html.link
 import kotlinx.html.option
+import kotlinx.html.script
 import kotlinx.html.select
+import kotlinx.html.span
 import kotlinx.html.submitInput
 import kotlinx.html.tbody
 import kotlinx.html.td
@@ -22,6 +26,7 @@ import kotlinx.html.title
 import kotlinx.html.tr
 import sh.zachwal.button.controller.Controller
 import sh.zachwal.button.roles.adminRoute
+import sh.zachwal.button.roles.contactRoute
 import sh.zachwal.button.sharedhtml.headSetup
 import sh.zachwal.button.sharedhtml.responsiveTable
 
@@ -35,48 +40,68 @@ class ContactPressStatsController @Inject constructor(
             get {
                 val range = TimeRange.fromParam(call.request.queryParameters["range"])
                 val stats = service.pressStats(range)
+                call.respondHtml { leaderboardPage(range, stats) }
+            }
+        }
+        contactRoute("/leaderboard") {
+            get {
+                val range = TimeRange.fromParam(call.request.queryParameters["range"])
+                val stats = service.pressStats(range)
+                call.respondHtml { leaderboardPage(range, stats) }
+            }
+        }
+    }
 
-                call.respondHtml {
-                    head {
-                        title { +"Contact Press Stats" }
-                        headSetup()
-                    }
-                    body {
-                        div(classes = "container") {
-                            h1(classes = "mt-4 text-center") { +"Contact Press Stats" }
-                            form(classes = "d-flex align-items-center my-3 px-2", method = FormMethod.get) {
-                                select(classes = "form-control flex-grow-1 mr-2") {
-                                    name = "range"
-                                    TimeRange.entries.forEach { tr ->
-                                        option {
-                                            value = tr.queryParam
-                                            selected = tr == range
-                                            +tr.label
-                                        }
-                                    }
+    private fun HTML.leaderboardPage(range: TimeRange, stats: List<ContactPressStat>) {
+        head {
+            title { +"Leaderboard" }
+            headSetup()
+            link(rel = "stylesheet", href = "/static/src/css/leaderboard.css", type = "text/css")
+        }
+        body {
+            div(classes = "container") {
+                h1(classes = "mt-4 text-center") { +"Leaderboard" }
+                form(classes = "my-3 px-2", method = FormMethod.get) {
+                    div(classes = "d-flex align-items-center") {
+                        select(classes = "form-control flex-grow-1 mr-2") {
+                            name = "range"
+                            TimeRange.entries.forEach { tr ->
+                                option {
+                                    value = tr.queryParam
+                                    selected = tr == range
+                                    +tr.label
                                 }
-                                submitInput(classes = "btn btn-primary") { value = "Go" }
                             }
-                            responsiveTable(classes = "mt-3") {
-                                thead {
-                                    tr {
-                                        th { +"Name" }
-                                        th { +"Presses" }
-                                    }
-                                }
-                                tbody {
-                                    stats.forEach { stat ->
-                                        tr {
-                                            td { +stat.contact.name }
-                                            td { +stat.count.toString() }
-                                        }
-                                    }
-                                }
+                        }
+                        submitInput(classes = "btn btn-primary") { value = "Go" }
+                    }
+                    div(classes = "text-center mt-1") {
+                        span(classes = "text-muted small") {
+                            attributes["id"] = "stale-indicator"
+                            attributes["style"] = "display:none"
+                            +"Hit Go to refresh"
+                        }
+                    }
+                }
+                responsiveTable(classes = "mt-3") {
+                    thead {
+                        tr {
+                            th { +"Name" }
+                            th { +"Presses" }
+                        }
+                    }
+                    tbody {
+                        stats.forEach { stat ->
+                            tr {
+                                attributes["style"] = "view-transition-name: contact-${stat.contact.id}"
+                                td { +stat.contact.name }
+                                td { +stat.count.toString() }
                             }
                         }
                     }
                 }
             }
+            script { src = "/static/src/js/leaderboard.js" }
         }
     }
 }
